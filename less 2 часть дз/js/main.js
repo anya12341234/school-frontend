bc2910fb624b75ec6992c7b2b0aea832be795570
Anya <putrova.13anya@gmail.com>
2022-08-30 17:04:40 +0300

    add

--- less 2 часть дз/js/main.js
+++ less 2 часть дз/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(".register_form label").on("click", function(e) {
+    $(".register_form input").on("click", function(e) {
      e.preventDefault();
      $(".form_row").toggleClass("active");
     });
